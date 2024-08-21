e4d5b58157ddbe9589a8b273d146bd282aae6bfb
Linda Hrdinová <hrlin@seznam.cz>
2024-08-21 23:31:16 +0200

    int -> long,

--- src/main/java/com/github/lindaHrdinova/box_packing/BoxPackingApplication.java
+++ src/main/java/com/github/lindaHrdinova/box_packing/BoxPackingApplication.java
@@ -15,7 +15,7 @@ public class BoxPackingApplication {
     private static final DecimalFormat df = new DecimalFormat("0.00");
 
     // Functions for calculating GCD using Euclidean algorithm
-    public static int gcd(int a, int b) {
+    public static long gcd(long a, long b) {
         if (b == 0) {
             return a;
         }
@@ -23,7 +23,7 @@ public class BoxPackingApplication {
     }
 
     // Functions to calculate LCM using GCD
-    public static int lcm(int a, int b) {
+    public static long lcm(long a, long b) {
         return (a * b) / gcd(a, b);
     }
 
@@ -35,7 +35,7 @@ public class BoxPackingApplication {
 
         double boxSize;
 
-        // Spliting input to array
+        // spliting input to array
         String[] allSidesStringArray = allProducts.split("\\s+");
 
         // three numbers were not entered for a product || no products have been entered
@@ -43,30 +43,36 @@ public class BoxPackingApplication {
             System.err.println("Incorrect input.");
         } else {
             // String[] -> Double[] * 100 -> int[]
-            int[] allSidesIntArray = Arrays.stream(allSidesStringArray)
+            long[] allSidesLongArray = Arrays.stream(allSidesStringArray)
                     .mapToDouble(Double::parseDouble)
                     .map(side -> side * 100)
-                    .mapToInt(side -> (int) Math.round(side))
+                    .mapToLong(side -> (long) Math.round(side))
                     .toArray();
 
-            System.out.println("Input: " + Arrays.toString(allSidesIntArray) + ", number of elements: " + allSidesIntArray.length);
+            System.out.println("Input: " + Arrays.toString(allSidesLongArray) + ", number of elements: " + allSidesLongArray.length);
 
             // LCM for all element array
-            for (int i = 0; i < (allSidesIntArray.length - 1); i++) {
-                System.out.println("lcm(" + allSidesIntArray[i] + ", " + allSidesIntArray[i+1] + ") = " + lcm(allSidesIntArray[i], allSidesIntArray[i + 1]));
-                boxSize = (double) lcm(allSidesIntArray[i], allSidesIntArray[i + 1]) / 100;
-                allSidesIntArray[i+1] = lcm(allSidesIntArray[i], allSidesIntArray[i + 1]);
+            for (int i = 0; i < (allSidesLongArray.length - 1); i++) {
+                System.out.println("lcm(" + allSidesLongArray[i] + ", " + allSidesLongArray[i+1] + ") = " + lcm(allSidesLongArray[i], allSidesLongArray[i + 1]));
 
-                if(boxSize >  10000000.00) {
-                    System.err.println("The box is too big.");
+                // Incorrect input range
+                if( ((allSidesLongArray[0] > 1000000000.00 ) || (allSidesLongArray[i + 1]) > 1000000000.00) || ((allSidesLongArray[0]) < 1) || ((allSidesLongArray[i + 1]) < 1)) {
+                    System.err.println("Incorrect input.");
                 }
-                else if (i == (allSidesIntArray.length - 2)) {
+
+                boxSize = (double) lcm(allSidesLongArray[i], allSidesLongArray[i + 1]) / 100;
+                allSidesLongArray[i+1] = lcm(allSidesLongArray[i], allSidesLongArray[i + 1]);
+
+
+                // Box size
+                if(boxSize > 10000000.00 || boxSize < 0.01) {
+                    System.err.println("The box is too big.");
+                    break;
+                }
+                else if (i == (allSidesLongArray.length - 2)) {
                     System.out.println("Box size: " + df.format(boxSize));
                 }
-
             }
         }
-
     }
-
 }
