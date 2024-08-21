967a092327ba98db56a7429b9c4f2d14e3861dd4
Linda Hrdinová <hrlin@seznam.cz>
2024-08-21 16:59:50 +0200

    box for 1 product

--- src/main/java/com/github/lindaHrdinova/box_packing/BoxPackingApplication.java
+++ src/main/java/com/github/lindaHrdinova/box_packing/BoxPackingApplication.java
@@ -2,11 +2,47 @@ package com.github.lindaHrdinova.box_packing;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.github.lindaHrdinova.box_packing.BoxingSize.*;
+import java.util.Scanner;
 
 @SpringBootApplication
 public class BoxPackingApplication {
+
+	// Functions for calculating GCD using Euclidean algorithm
+	public static int gcd(int a, int b) {
+		if (b == 0) {
+			return a;
+		}
+		return gcd(b, a % b);
+	}
+
+	// Functions to calculate LCM using GCD
+	public static int lcm(int a, int b) {
+		return (a * b) / gcd(a, b);
+	}
+
 	public static void main(String[] args) {
-		System.out.println("hello world");
+		System.out.println("Product size:");
+		Scanner scanner = new Scanner(System.in);
+		String oneBox = (scanner.nextLine());
+		System.out.println(oneBox);
+
+
+		String[] strArray = oneBox.split("\\s+");
+		BoxingSize box1 = new BoxingSize();
+
+		box1.setSideA(Double.parseDouble(strArray[0]));
+		box1.setSideB(Double.parseDouble(strArray[1]));
+		box1.setSideC(Double.parseDouble(strArray[2]));
+
+		System.out.println("strana a: " + box1.getSideA());
+		System.out.println("strana b: " + box1.getSideB());
+		System.out.println("strana c: " + box1.getSideC());
+
+		double sideAB = (double) (lcm((int) box1.getSideA()*100, (int) box1.getSideB()*100))/100;
+		System.out.println("lcm of a and b is: " + sideAB);
+		double sideABC = (double) (lcm((int) sideAB*100, (int) box1.getSideC()*100))/100;
+		System.out.println("lcm of ab and c is: " + sideABC);
 	}
 
 }
--- src/main/java/com/github/lindaHrdinova/box_packing/BoxingSize.java
+++ src/main/java/com/github/lindaHrdinova/box_packing/BoxingSize.java
@@ -1,2 +1,42 @@
-package com.github.lindaHrdinova.box_packing;public class BoxingSize {
+package com.github.lindaHrdinova.box_packing;
+
+public class BoxingSize {
+    private double sideA;
+    private double sideB;
+    private double sideC;
+    private double finalSize;
+
+    public double getSideA() {
+        return sideA;
+    }
+
+    public String setSideA(double sideA) {
+        this.sideA = sideA;
+        return null;
+    }
+
+    public double getSideB() {
+        return sideB;
+
+    }
+
+    public void setSideB(double sideB) {
+        this.sideB = sideB;
+    }
+
+    public double getSideC() {
+        return sideC;
+    }
+
+    public void setSideC(double sideC) {
+        this.sideC = sideC;
+    }
+
+    public double getFinalSize() {
+        return finalSize;
+    }
+
+    public void setFinalSize(double finalSize) {
+        this.finalSize = finalSize;
+    }
 }
